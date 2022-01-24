eafe9928338458bf3b5e80ceba1ee1f95066242b
YongQiang <chienyq221@gmail.com>
2022-01-24 16:30:17 +0800

    added 2 buttons to navigate from homepage

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,8 +1,23 @@
 import React from "react";
+import {Link} from 'react-router-dom'
 
 const HomePage = () => {
+
     return (
-        <p>Home Page</p>
+        <div>
+            <p>Home Page</p>
+            <Link to="/call">
+            <button type="button">
+                call
+            </button>
+            </Link>
+
+            <Link to="/start">
+            <button type="button">
+                start
+            </button>
+            </Link>
+        </div>
     )
 }
 
